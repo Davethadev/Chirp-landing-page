11681b6054edb57ea07fb0efc00e493f15519e62
David Umanah <david_umanah@yahoo.com>
2022-08-22 11:25:25 +0100

    fix style

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-const hamburgerMenu = document.getElementById('hamburger-menu')
-hamburgerMenu.addEventListener('click', display)
-
-function display() {
-    var x = document.getElementById("myLinks")
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-    // const div = document.createElement('div')
-    // div.setAttribute('style', 'width: 100px; height: auto;')
-    // div.innerHTML = `
-    // <a href="#">Home</a>
-    // <a href="#">Pricing</a>
-    // <a href="#">FAQ</a>
-    // `
-    // hamburgerMenu.appendChild(div)
-    //hamburgerMenu.style.backgroundColor = 'blue';
-}
+const topNav = document.querySelector('.top-nav')
+topNav.addEventListener('click', function() {
+    this.classList.toggle('active')
+})
